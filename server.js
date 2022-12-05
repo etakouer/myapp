adfeefaf6d0c43b83941c45988786bbaab4cb9f7
etakouer <hoctac@gmail.com>
2022-12-05 12:02:12 +0100

    add ci

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ import express from "express"
 const app = express()
 
 app.use("/", (req, res) => {
-	res.send("hey ! it's myapp")
+	res.send("Hey ! it's myapp " + crypto.randomUUID().slice(-8))
 })
 
 app.listen(4000, () => {
 	console.log("I am running on port 4000 !")
 })
+
+setInterval(() => {
+  console.log(new Date().toISOString().slice(11,19) + " > I am doing somethings ...");
+}, 2000)
